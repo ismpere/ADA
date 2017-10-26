88824823206f7deb1fb5d52b5f8f40ef07db3701
Ismael Pérez Martín <ismaelprzm@gmail.com>
2017-10-26 18:02:12 +0200

    Añado parte del backtracking

--- src/Practica2.java
+++ src/Practica2.java
@@ -19,7 +19,9 @@ public class Practica2 {
 		uno.add("A");
 		uno.add("G");
 		uno.add("C");
+		uno.add("X");
 		uno.add("G");
+		
 		dos.add("G");
 		dos.add("C");
 		dos.add("G");
@@ -28,29 +30,35 @@ public class Practica2 {
 		dos.add("A");
 		dos.add("T");
 		dos.add("G");
-		System.out.println(funcion(uno, dos).toString());;
+		System.out.println(funcion(uno, dos).toString());
+		System.out.println(posV1.toString());
+		System.out.println(posV2.toString());
+	}
+	public static ArrayList<String> cadena(ArrayList<String> x, ArrayList<String> y){
+		
+		return funcion(x,y);
 	}
 	public static ArrayList<String> funcion (ArrayList<String> x, ArrayList<String> y){
 		  int j = 0;
 		  ArrayList<String> listaFin = new ArrayList<String>();
+		  ArrayList<String> sublist1 = new ArrayList<String>(x.subList(1,x.size()));
+		  ArrayList<String> sublist2 = new ArrayList<String>();
 		  if(x.isEmpty() || y.isEmpty()){
 		    return listaFin;
 		  }else{
+			prof1++;
 		    while((j<y.size()) && (x.get(0)!=y.get(j))){
 		      j++;
 		    }
 		    if(j<y.size()){
-		      listaFin.add(x.get(0));
-		    }
-		    ArrayList<String> sublist1 = new ArrayList<String>(x.subList(1,x.size()));
-		    ArrayList<String> sublist2 = new ArrayList<String>();
-		    if(j<y.size()){
-		    	if((j+1)==y.size()){
-		    		prof1 --;
+		    	listaFin.add(x.get(0));
+		    	prof2+=j;
+		    	posV1.add(prof1-1);
+		    	posV2.add(prof2);
+		    	if(j==y.size()-1){
 		    		return listaFin;
 		    	}else{
-		    		prof1 ++;
-		    		prof2 = prof2 +j;
+		    		prof2++;
 		    		sublist2 = new ArrayList<String>(y.subList(j+1, y.size()));
 		    	}
 		    }else{
